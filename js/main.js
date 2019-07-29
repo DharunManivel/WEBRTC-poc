5410c24f805fc7a8e2f586f825f154be40227fd4
DharunManivel <mdharun96@gmail.com>
2019-07-29 20:18:30 +0530

    Bitrate Reduced in SDP

--- js/main.js
+++ js/main.js
@@ -117,7 +117,7 @@ socket.on('message', function (message) {
       pc.setRemoteDescription(new RTCSessionDescription(message));
       doAnswer();
     } else if (message.type === 'answer' && isStarted) {
-console.log("----ANSWER----",message)
+console.log("----ANSWER SDP----",message.sdp)
       pc.setRemoteDescription(new RTCSessionDescription({
         "sdp":message.sdp,
         "type":message.type
@@ -260,9 +260,10 @@ function setLocalAndSendMessage(sessionDescription) {
   console.log("SESSION",session);
   sessionDescription.session = session
   pc.setLocalDescription(sessionDescription);
-  console.log('setLocalAndSendMessage sending message', sessionDescription);
+ // console.log('setLocalAndSendMessage sending message', sessionDescription);
+ console.log('SDP in setLOcal', sessionDescription.sdp);
   sendMessage({
-    "sdp": sessionDescription.sdp,
+    "sdp":setMediaBitrates(sessionDescription.sdp),
     "type": sessionDescription.type,
     "session": session
   });
@@ -369,4 +370,46 @@ function stop() {
   isStarted = false;
   pc.close();
   pc = null;
+}
+
+function setMediaBitrates(sdp) {
+  return setMediaBitrate(setMediaBitrate(sdp, "video", 500), "audio", 50);
+}
+
+function setMediaBitrate(sdp, media, bitrate) {
+  var lines = sdp.split("\n");
+  var line = -1;
+  for (var i = 0; lines.length; i++) {
+    if (lines[i].indexOf("m="+media) === 0) {
+      line = i;
+      break;
+    }
+  }
+  if (line === -1) {
+    console.debug("Could not find the m line for", media);
+    return sdp;
+  }
+  console.debug("Found the m line for", media, "at line", line);
+
+  // Pass the m line
+  line++;
+
+  // Skip i and c lines
+  while(lines[line].indexOf("i=") === 0 || lines[line].indexOf("c=") === 0) {
+    line++;
+  }
+
+  // If we're on a b line, replace it
+  if (lines[line].indexOf("b") === 0) {
+    console.debug("Replaced b line at line", line);
+    lines[line] = "b=AS:"+bitrate;
+    return lines.join("\n");
+  }
+  
+  // Add a new b line
+  console.debug("Adding new b line before line", line);
+  var newLines = lines.slice(0, line)
+  newLines.push("b=AS:"+bitrate)
+  newLines = newLines.concat(lines.slice(line, lines.length))
+  return newLines.join("\n")
 }
